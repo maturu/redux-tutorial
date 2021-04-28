f285c6c6b0229ecd5677b0a9631acd85db0ab851
maturu <maturu.410@gmail.com>
2021-04-28 17:26:05 +0900

    redux logger & thunk & async tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,4 +1,26 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import axios from "axios";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null
+};
+
+const testReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "FETCH_USERS_START":
+      return {...state, fetching: true};
+    case "FETCH_USERS_ERROR":
+      return {...state, fetching: false, error: action.payload};
+    case "RECEIVE_USERS":
+      return {...state, fetching: false, fetched: true, users: action.payload};
+  }
+  return state;
+}
 
 const userReducer = (state = {}, action) => {
   switch(action.type) {
@@ -17,22 +39,39 @@ const tweetsReducer = (state = {}, action) => {
     case "ADD_TWEET":
       state = { ...state, id: Date.now(), text: action.payload };
       break;
+    case "ERROR":
+      throw new Error("It's error!!!!");
   }
   return state;
 }
 
 const reducers = combineReducers({
+  test: testReducer,
   user: userReducer,
   tweets: tweetsReducer
 });
 
-const store = createStore(reducers);
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch(e) {
+    console.log("Error was occured", e);
+  }
+}
+
+const middleware = applyMiddleware(thunk, createLogger(), error);
+const store = createStore(reducers, {}, middleware);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
-store.dispatch({ type: "CHANGE_NAME", payload: "Maturu" });
-store.dispatch({ type: "CHANGE_AGE", payload: 35 });
-store.dispatch({ type: "ADD_TWEET", payload: "OMG LIKE LOL" });
-store.dispatch({ type: "ADD_TWEET", payload: "I am so like seriously like totally like right now" });
+store.dispatch((dispatch) => {
+  dispatch({ type: "FETCH_USERS_START" });
+  axios.get("http://localhost:18080").then((response) => {
+    dispatch({ type: "RECEIVE_USERS", payload: response.data });
+  }).catch((err) => {
+    dispatch({ type: "FETCH_USERS_ERROR", payload: err });
+  });
+});
+
